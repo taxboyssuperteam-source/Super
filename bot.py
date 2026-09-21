352e95ab5f68ea2d698b991324eebdc3d69429cf
taxboyssuperteam-source <taxboyssuperteam@gmail.com>
2026-09-21 22:15:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import asyncio
+mimport asyncio
 import logging
 import subprocess
 from datetime import datetime, timedelta, timezone
@@ -32,8 +32,8 @@ logger = logging.getLogger(__name__)
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-MONGODB_URI = os.getenv("MONGODB_URI")
-DATABASE_NAME = os.getenv("DATABASE_NAME", "attack_bot")
+MONGODB_URI = os.getenv("https://mangob.st/attackHEX.09.01")
+DATABASE_NAME = os.getenv("mxteam", "attack_bot")
 API_URL = os.getenv("https://kimstress.st")
 API_KEY = os.getenv("157c5d71b01f0ebb3f606860fb856c43c47125c7a94e0ba015bd15c59df4e2c1")
 ADMIN_IDS = [int(id.strip()) for id in os.getenv("ADMIN_IDS", "1793697840").split(",")]
